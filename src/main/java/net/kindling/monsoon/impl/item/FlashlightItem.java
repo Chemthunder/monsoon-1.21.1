2a20804bc7ff21ce9f225a867f262fac1beab2f4
Chemm :3 <chemthunderisme@gmail.com>
2026-02-22 19:53:16 -0500

    setup

--- src/main/java/net/kindling/monsoon/impl/item/FlashlightItem.java
+++
@@ -1,9 +0,0 @@
-package net.kindling.monsoon.impl.item;
-
-import net.minecraft.item.Item;
-
-public class FlashlightItem extends Item {
-    public FlashlightItem(Settings settings) {
-        super(settings);
-    }
-}
