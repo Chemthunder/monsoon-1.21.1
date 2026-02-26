f95f1ee3449da6994b7b2d6d2263cab5c22eaf50
thatoneaiguy <animations.by.alby@gmail.com>
2026-02-26 16:35:51 +0000

    crisps now stack to 99

--- src/main/java/net/kindling/monsoon/mixin/ItemMixin.java
+++ src/main/java/net/kindling/monsoon/mixin/ItemMixin.java
@@ -1,22 +1,30 @@
 package net.kindling.monsoon.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.kindling.monsoon.impl.item.CrispItem;
+import net.kindling.monsoon.mixin.access.ItemComponentAccessor;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Item.class)
 public abstract class ItemMixin {
 
-    @ModifyReturnValue(method = "getMaxCount", at = @At("RETURN"))
-    private int modifyMaxCount(int original) {
-        Item item = (Item) (Object) this;
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void modifyMaxCount(Item.Settings settings, CallbackInfo ci) {
+        Item self = (Item)(Object)this;
+        ComponentMap stackMap = self.getComponents();
 
-        if (item instanceof CrispItem) {
-            return 128;
+        Integer sizeMap = stackMap.get(DataComponentTypes.MAX_STACK_SIZE);
+
+        if (sizeMap != null && sizeMap == 64) {
+            ComponentMap.Builder builder = ComponentMap.builder();
+            builder.addAll(stackMap);
+            builder.add(DataComponentTypes.MAX_STACK_SIZE, 99); // max is 99
+            ((ItemComponentAccessor) self).setComponents(builder.build());
         }
-
-        return original;
     }
 }
